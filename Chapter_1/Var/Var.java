b962459b632360dcb3e7a0c16e739873fa825ad8
Katleho Khanye <kakhanye@gmail.com>
2023-08-04 08:42:01 +0200

    Working with local var variable.

---
+++ Chapter_1/Var/Var.java
@@ -0,0 +1,8 @@
+public class Var {
+  public static void main(String[] args) {
+    var num1 = 1;
+    var num2 = 2;
+    var sum = num1 + num2;
+    System.out.println("Sum : " + sum);
+  }
+}
---
+++ Chapter_1/Var/Var2.java
@@ -0,0 +1,10 @@
+public class Var2 {
+  private static int getNumber() {
+    return 3;
+  }
+
+  public static void main(String[] args) {
+    var num = getNumber();
+    System.out.println("Number is : " + num);
+  }
+}
