a3ff2b2f0691ea5d1daad66fdb15960859702e9a
Katleho Khanye <kakhanye@gmail.com>
2023-08-25 03:55:57 +0200

    Negating exercise

---
+++ Chapter_2/Negating/Negating.java
@@ -0,0 +1,8 @@
+public class Negating {
+  public static void main(String[] args) {
+    int num = 5;
+    System.out.println("Number is " + num);
+    int negatedNumber = -num;
+    System.out.println("Negated number is " + negatedNumber);
+  }
+}
